a4a0a02e37d5ac79c5ed3256894383396838fb04
Alex Stringer <awstringer@gmail.com>
2020-10-28 17:01:46 -0400

    added 2d example tests

--- tests/testthat/setup-01-optimization.R
+++ tests/testthat/setup-01-optimization.R
@@ -2,9 +2,11 @@ library(aghq)
 logfteta <- function(eta,y) {
   sum(y) * eta - (length(y) + 1) * exp(eta) - sum(lgamma(y+1)) + eta
 }
+truelogint <- function(y) lgamma(1 + sum(y)) - (1 + sum(y)) * log(length(y) + 1) - sum(lgamma(y+1))
 set.seed(84343124)
 y <- rpois(10,5) # Mode should be sum(y) / (10 + 1)
 truemode <- log((sum(y) + 1)/(length(y) + 1))
+truelognormconst <- truelogint(y)
 
 objfunc <- function(x) logfteta(x,y)
 funlist <- list(
@@ -29,6 +31,7 @@ norm_bfgs_3 <- normalize_logpost(opt_bfgs,3,1)
 norm_bfgs_5 <- normalize_logpost(opt_bfgs,5,1)
 norm_bfgs_7 <- normalize_logpost(opt_bfgs,7,1)
 
+
 ## A 2d example ##
 logfteta2d <- function(eta,y) {
   # eta is now (eta1,eta2)
@@ -50,6 +53,7 @@ n <- n1+n2
 y1 <- rpois(n1,5)
 y2 <- rpois(n2,5)
 truemode2d <- c(log((sum(y1) + 1)/(length(y1) + 1)),log((sum(y2) + 1)/(length(y2) + 1)))
+truelognormconst2d <- truelogint(y1) + truelogint(y2)
 
 objfunc2d <- function(x) logfteta2d(x,c(y1,y2))
 funlist2d <- list(
@@ -58,6 +62,18 @@ funlist2d <- list(
   he = function(x) numDeriv::hessian(objfunc2d,x)
 )
 
-opt_sparsetrust <- optimize_theta(funlist2d,c(1.5,1.5))
-opt_trust <- optimize_theta(funlist2d,c(1.5,1.5),control = list(method = "trust"))
-opt_bfgs <- optimize_theta(funlist2d,c(1.5,1.5),control = list(method = "BFGS"))
+opt_sparsetrust_2d <- optimize_theta(funlist2d,c(1.5,1.5))
+opt_trust_2d <- optimize_theta(funlist2d,c(1.5,1.5),control = list(method = "trust"))
+opt_bfgs_2d <- optimize_theta(funlist2d,c(1.5,1.5),control = list(method = "BFGS"))
+
+norm_sparse_2d_3 <- normalize_logpost(opt_sparsetrust_2d,3,1)
+norm_sparse_2d_5 <- normalize_logpost(opt_sparsetrust_2d,5,1)
+norm_sparse_2d_7 <- normalize_logpost(opt_sparsetrust_2d,7,1)
+
+norm_trust_2d_3 <- normalize_logpost(opt_trust_2d,3,1)
+norm_trust_2d_5 <- normalize_logpost(opt_trust_2d,5,1)
+norm_trust_2d_7 <- normalize_logpost(opt_trust_2d,7,1)
+
+norm_bfgs_2d_3 <- normalize_logpost(opt_bfgs_2d,3,1)
+norm_bfgs_2d_5 <- normalize_logpost(opt_bfgs_2d,5,1)
+norm_bfgs_2d_7 <- normalize_logpost(opt_bfgs_2d,7,1)
--- tests/testthat/test-02-normalization.R
+++ tests/testthat/test-02-normalization.R
@@ -1,6 +1,7 @@
 context("Normalization")
 
 test_that("Normalization works",{
+    # Return correct object
     expect_length(norm_sparse_3,3)
     expect_length(norm_sparse_5,3)
     expect_length(norm_sparse_7,3)
@@ -9,10 +10,23 @@ test_that("Normalization works",{
     expect_length(norm_trust_5,3)
     expect_length(norm_trust_7,3)
 
-    expect_length(norm_trust_3,3)
-    expect_length(norm_trust_5,3)
-    expect_length(norm_trust_7,3)
+    expect_length(norm_bfgs_3,3)
+    expect_length(norm_bfgs_5,3)
+    expect_length(norm_bfgs_7,3)
 
+    expect_length(norm_sparse_2d_3,3)
+    expect_length(norm_sparse_2d_5,3)
+    expect_length(norm_sparse_2d_7,3)
+
+    expect_length(norm_trust_2d_3,3)
+    expect_length(norm_trust_2d_5,3)
+    expect_length(norm_trust_2d_7,3)
+
+    expect_length(norm_bfgs_2d_3,3)
+    expect_length(norm_bfgs_2d_5,3)
+    expect_length(norm_bfgs_2d_7,3)
+
+    # Correct number of nodes
     expect_equal(nrow(norm_sparse_3$nodesandweights),3)
     expect_equal(nrow(norm_sparse_5$nodesandweights),5)
     expect_equal(nrow(norm_sparse_7$nodesandweights),7)
@@ -21,10 +35,23 @@ test_that("Normalization works",{
     expect_equal(nrow(norm_trust_5$nodesandweights),5)
     expect_equal(nrow(norm_trust_7$nodesandweights),7)
 
-    expect_equal(nrow(norm_trust_3$nodesandweights),3)
-    expect_equal(nrow(norm_trust_5$nodesandweights),5)
-    expect_equal(nrow(norm_trust_7$nodesandweights),7)
+    expect_equal(nrow(norm_bfgs_3$nodesandweights),3)
+    expect_equal(nrow(norm_bfgs_5$nodesandweights),5)
+    expect_equal(nrow(norm_bfgs_7$nodesandweights),7)
 
+    expect_equal(nrow(norm_sparse_2d_3$nodesandweights),3^2)
+    expect_equal(nrow(norm_sparse_2d_5$nodesandweights),5^2)
+    expect_equal(nrow(norm_sparse_2d_7$nodesandweights),7^2)
+
+    expect_equal(nrow(norm_trust_2d_3$nodesandweights),3^2)
+    expect_equal(nrow(norm_trust_2d_5$nodesandweights),5^2)
+    expect_equal(nrow(norm_trust_2d_7$nodesandweights),7^2)
+
+    expect_equal(nrow(norm_bfgs_2d_3$nodesandweights),3^2)
+    expect_equal(nrow(norm_bfgs_2d_5$nodesandweights),5^2)
+    expect_equal(nrow(norm_bfgs_2d_7$nodesandweights),7^2)
+
+    # Normconst is not infinite
     expect_false(is.infinite(norm_sparse_3$lognormconst))
     expect_false(is.infinite(norm_sparse_5$lognormconst))
     expect_false(is.infinite(norm_sparse_7$lognormconst))
@@ -33,9 +60,53 @@ test_that("Normalization works",{
     expect_false(is.infinite(norm_trust_5$lognormconst))
     expect_false(is.infinite(norm_trust_7$lognormconst))
 
-    expect_false(is.infinite(norm_trust_3$lognormconst))
-    expect_false(is.infinite(norm_trust_5$lognormconst))
-    expect_false(is.infinite(norm_trust_7$lognormconst))
+    expect_false(is.infinite(norm_bfgs_3$lognormconst))
+    expect_false(is.infinite(norm_bfgs_5$lognormconst))
+    expect_false(is.infinite(norm_bfgs_7$lognormconst))
 
+    expect_false(is.infinite(norm_sparse_2d_3$lognormconst))
+    expect_false(is.infinite(norm_sparse_2d_5$lognormconst))
+    expect_false(is.infinite(norm_sparse_2d_7$lognormconst))
+
+    expect_false(is.infinite(norm_trust_2d_3$lognormconst))
+    expect_false(is.infinite(norm_trust_2d_5$lognormconst))
+    expect_false(is.infinite(norm_trust_2d_7$lognormconst))
+
+    expect_false(is.infinite(norm_bfgs_2d_3$lognormconst))
+    expect_false(is.infinite(norm_bfgs_2d_5$lognormconst))
+    expect_false(is.infinite(norm_bfgs_2d_7$lognormconst))
+
+
+
+    # Require integer number of quad points
     expect_error(normalize_logpost(opt_sparsetrust,2.5))
+
+    # Normconst actually equals the correct thing
+    expect_equal(round(norm_sparse_3$lognormconst,2),round(truelognormconst,2))
+    expect_equal(round(norm_sparse_5$lognormconst,3),round(truelognormconst,3))
+    expect_equal(round(norm_sparse_7$lognormconst,3),round(truelognormconst,3))
+
+    expect_equal(round(norm_trust_3$lognormconst,2),round(truelognormconst,2))
+    expect_equal(round(norm_trust_5$lognormconst,3),round(truelognormconst,3))
+    expect_equal(round(norm_trust_7$lognormconst,3),round(truelognormconst,3))
+
+    expect_equal(round(norm_bfgs_3$lognormconst,2),round(truelognormconst,2))
+    expect_equal(round(norm_bfgs_5$lognormconst,3),round(truelognormconst,3))
+    expect_equal(round(norm_bfgs_7$lognormconst,3),round(truelognormconst,3))
+
+    expect_equal(round(norm_sparse_2d_3$lognormconst,1),round(truelognormconst2d,1))
+    expect_equal(round(norm_sparse_2d_5$lognormconst,2),round(truelognormconst2d,2))
+    expect_equal(round(norm_sparse_2d_7$lognormconst,2),round(truelognormconst2d,2))
+
+    expect_equal(round(norm_trust_2d_3$lognormconst,1),round(truelognormconst2d,1))
+    expect_equal(round(norm_trust_2d_5$lognormconst,2),round(truelognormconst2d,2))
+    expect_equal(round(norm_trust_2d_7$lognormconst,2),round(truelognormconst2d,2))
+
+    expect_equal(round(norm_bfgs_2d_3$lognormconst,1),round(truelognormconst2d,1))
+    expect_equal(round(norm_bfgs_2d_5$lognormconst,2),round(truelognormconst2d,2))
+    expect_equal(round(norm_bfgs_2d_7$lognormconst,2),round(truelognormconst2d,2))
+
+
+
+
 })
